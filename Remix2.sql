fff78e17345bba059dacd85bfe73a2026a7a4a77
Jouse Márquez <mquezflores@gmail.com>
2022-11-30 11:28:44 +0100

    Add files via upload

---
+++ Remix2.sql
@@ -0,0 +1,28 @@
+/*A la atención de su señoría, aqueste enunciado ha sido preparado para que lo entienda vuesa merced.
+Espero que sea de su gusto y no genere ambigüedades, puesto que este servidor suyo lo ha preparado
+con grande cuidado. Si así no fuere, pardiez que me lleve el diablo y se coma mi alma.*/
+
+-- 1. Por cada compañía de envíos, mostrar su nombre y contar el número de pedidos
+-- que ha enviado.
+
+-- 2. Mostrar el nombre de los países a los que pertenecen los distintos clientes y empleados,
+-- de forma que no se repitan.
+
+-- 3. Contar los distintos ID de territorios.
+
+-- 4. Contar el número de categorías cuyo nombre empieza por C y termina por S,
+-- dando un alias a la columna.
+
+-- 5. Mostrar los distintos territorios, adjuntando también la descripción de región (RegionDescription).
+
+-- 6. Contar el número de filas de la tabla customerdemographics.
+
+-- 7. Mostrar los nombres de ciudades de los proveedores en orden alfabetico inverso, pero solo
+-- para aquellas cuyo nombre de ciudad comienza de la A a la C incluidas ambas letras.
+
+-- 8. Por cada nombre de compañía de envíos contar el número de distintos productos 
+-- enviados.
+-- Es decir, que si una compañía de envíos envía una bata, tres bicis y siete jerseys de cuello
+-- de pico estampados a un cliente zamorano y luego la misma compañía de envíos envía
+-- una bata a un cliente jienense, esa 
+-- compañía de envíos (que Dios la guarde en su alma) habría enviado tres productos distintos.
