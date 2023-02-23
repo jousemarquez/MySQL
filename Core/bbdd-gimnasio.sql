4f6d0f63e288f6e12bf77a63ee10c15c5d253529
jousemarquez <mquezflores@gmail.com>
2023-02-23 10:16:50 +0100

    Actualización BBDD

--- Core/bbdd-gimnasio.sql
+++ Core/bbdd-gimnasio.sql
@@ -1,31 +1,23 @@
 /*
 Gimnasio DDBB
 
-Un gimnasio desea crear una aplicación para permitir que los miembros se
-registren en las clases. El estudio tiene una variedad de tipos de clases y
-varios instructores diferentes.
+Un gimnasio desea crear una aplicación para permitir que los miembros se registren en las clases.
+El estudio tiene una variedad de tipos de clases y varios instructores diferentes.
 
 El modelado que se plantee, debe satisfacer los siguientes requisitos:
 
 - Cada socio del gimnasio puede realizar múltiples reservas de clases (yoga, pilates, GAP, cycling, spinning, crossfit, etc)
 - Cada reserva pertenece a un solo miembro y a una sola clase programada. (por ejemplo, el miembro n.º 1 está inscrito en 
 la clase programada n.º 211, "Spinning (60 minutos) con el monitor Raúl Servet el miércoles 25 de agosto de 2021 de 2 a 3 p. m.").
-
 - El horario de las clases programadas representa el horario general del gimnasio.
 Incluye las combinaciones únicas de clases, instructores y fecha/hora.
-
 - Cada clase programada puede aparecer en el horario de clases varias veces (por ejemplo, "Yoga avanzado
 (40 minutos)" está programada los lunes, miércoles y viernes a las 8 a. m. y 6 p. m.).
-
 - Cada clase también pertenece a un solo tipo (por ejemplo, "Yoga avanzado (40 minutos)" pertenece a "Yoga", al igual que
 “Yoga iniciación (20 minutos)”).
-
 - Cada tipo (por ejemplo, yoga, pilates, spinning) tiene muchas clases.
-
 - Cada instructor, tiene muchas clases programadas.
 
-En 3º Forma Normal, describa las entidades y
-sus atributos a modo de tablas y columnas, así como sus relaciones y
-cardinalidades. El modelado debe responder de manera eficiente a las preguntas
-derivadas de los requisitos con anterioridad.
+En 3º Forma Normal, describa las entidades y sus atributos a modo de tablas y columnas, así como sus relaciones y
+cardinalidades. El modelado debe responder de manera eficiente a las preguntas derivadas de los requisitos con anterioridad.
 */
--- Core/bbdd-socialnetwork.sql
+++ Core/bbdd-socialnetwork.sql
@@ -11,46 +11,36 @@ DROP DATABASE IF EXISTS SocialNetwork;
 CREATE DATABASE SocialNetwork;
 USE SocialNetwork;
 
-DROP TABLE IF EXISTS Countries;
-CREATE TABLE Countries (
+DROP TABLE IF EXISTS RRSS_Countries;
+CREATE TABLE RRSS_Countries (
 country_id BIGINT NOT NULL AUTO_INCREMENT,
 name VARCHAR(100) NOT NULL,
 PRIMARY KEY (`country_id`),
 UNIQUE KEY (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- DROP DATA COUNTRIES
-INSERT INTO Countries (country_id, name) VALUES (0,"Spain");
 
-DROP TABLE IF EXISTS Cities;
-CREATE TABLE Cities (
+
+DROP TABLE IF EXISTS RRSS_Cities;
+CREATE TABLE RRSS_Cities (
 city_id BIGINT NOT NULL AUTO_INCREMENT,
 name VARCHAR(100) NOT NULL,
 country_id BIGINT NOT NULL,
 PRIMARY KEY (`city_id`),
 UNIQUE KEY (`name`),
-FOREIGN KEY (`country_id`) REFERENCES Countries(`country_id`)
+FOREIGN KEY (`country_id`) REFERENCES RRSS_Countries(`country_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- DROP DATA COUNTRIES
-INSERT INTO Cities (city_id, name, country_id) VALUES (0,"Sevilla",0);
-
-DROP TABLE IF EXISTS Genders;
-CREATE TABLE Genders(
+DROP TABLE IF EXISTS RRSS_Genders;
+CREATE TABLE RRSS_Genders(
 gender_id BIGINT NOT NULL AUTO_INCREMENT,
 name VARCHAR(100) NOT NULL,
 PRIMARY KEY (`gender_id`),
 UNIQUE KEY (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- DROP DATA GENDERS
-INSERT INTO Genders (gender_id, name)
-VALUES (0,"Male"),
-(1,"Female"),
-(2,"Helicopter");
-
-DROP TABLE IF EXISTS Users;
-CREATE TABLE IF NOT EXISTS Users(
+DROP TABLE IF EXISTS RRSS_Users;
+CREATE TABLE IF NOT EXISTS RRSS_Users(
 user_id BIGINT NOT NULL AUTO_INCREMENT,
 email VARCHAR(255) NOT NULL,
 username VARCHAR(50) NOT NULL,
@@ -64,19 +54,14 @@ date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 date_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 active ENUM('yes','no') NOT NULL DEFAULT 'no',
 PRIMARY KEY (`user_id`),
-FOREIGN KEY (`gender_id`) REFERENCES Genders(`gender_id`),
-FOREIGN KEY (`city_id`) REFERENCES Cities(`city_id`),
+FOREIGN KEY (`gender_id`) REFERENCES RRSS_Genders(`gender_id`),
+FOREIGN KEY (`city_id`) REFERENCES RRSS_Cities(`city_id`),
 UNIQUE KEY (`email`),
 UNIQUE KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- DROP DATA USERS
-INSERT INTO Users (email,username,password,real_name,gender_id,city_id,profile_picture_url,birth_date,date_created,date_updated)
-VALUES ("jouse@socialnetwork.com","Quorom","t0nt03lqu3l0l34","Jouse MF",0,0,NULL,DATE '1987-01-23',now(),now()),
-("josemari@socialnetwork.com","JM","t0nt03lqu3l0l34","JM AMF",0,0,NULL,DATE '1987-01-23',now(),now());
-
-DROP TABLE IF EXISTS Posts;
-CREATE TABLE Posts(
+DROP TABLE IF EXISTS RRSS_Posts;
+CREATE TABLE RRSS_Posts(
 post_id BIGINT NOT NULL AUTO_INCREMENT,
 user_id BIGINT NOT NULL,
 comment TEXT NULL,
@@ -84,14 +69,10 @@ type ENUM('image', 'video'),
 date_created DATE NOT NULL,
 date_updated DATE,
 PRIMARY KEY (`post_id`),
-FOREIGN KEY (`user_id`) REFERENCES Users(`user_id`)
+FOREIGN KEY (`user_id`) REFERENCES RRSS_Users(`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- DROP DATA POSTS
-INSERT INTO Posts (post_id, user_id, comment, type, date_created, date_updated)
-VALUES (0,1,"Hello world","image",now(),now());
-
-CREATE TABLE Comments(
+CREATE TABLE RRSS_Comments(
 comment_id BIGINT NOT NULL AUTO_INCREMENT,
 post_id BIGINT NOT NULL,
 user_id BIGINT NOT NULL,
@@ -99,23 +80,38 @@ content TEXT NOT NULL,
 date_created DATE NOT NULL,
 date_updated DATE,
 PRIMARY KEY (`comment_id`),
-FOREIGN KEY (`post_id`) REFERENCES Posts(`post_id`),
-FOREIGN KEY (`user_id`) REFERENCES Users(`user_id`)
+FOREIGN KEY (`post_id`) REFERENCES RRSS_Posts(`post_id`),
+FOREIGN KEY (`user_id`) REFERENCES RRSS_Users(`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- DROP DATA COMMENTS
-INSERT INTO Comments (comment_id, post_id, user_id, content, date_created, date_updated)
-VALUES (0,0,1,"Love your bald head",now(),now());
-
-CREATE TABLE Followings(
+CREATE TABLE RRSS_Followings(
 user_id BIGINT NOT NULL,
 following_id BIGINT NOT NULL,
 date_created DATE NOT NULL,
 PRIMARY KEY (`user_id`),
-FOREIGN KEY (`user_id`) REFERENCES Users(`user_id`),
-FOREIGN KEY (`following_id`) REFERENCES Users(`user_id`)
+FOREIGN KEY (`user_id`) REFERENCES RRSS_Users(`user_id`),
+FOREIGN KEY (`following_id`) REFERENCES RRSS_Users(`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- DROP DATA COUNTRIES
+INSERT INTO RRSS_Countries (country_id, name) VALUES (0,"Spain");
+-- DROP DATA COUNTRIES
+INSERT INTO RRSS_Cities (city_id, name, country_id) VALUES (0,"Sevilla",0);
+-- DROP DATA GENDERS
+INSERT INTO RRSS_Genders (gender_id, name)
+VALUES (0,"Male"),
+(1,"Female"),
+(2,"Helicopter");
+-- DROP DATA USERS
+INSERT INTO RRSS_Users (email,username,password,real_name,gender_id,city_id,profile_picture_url,birth_date,date_created,date_updated)
+VALUES ("jouse@socialnetwork.com","Quorom","t0nt03lqu3l0l34","Jouse MF",0,0,NULL,DATE '1987-01-23',now(),now()),
+("josemari@socialnetwork.com","JM","t0nt03lqu3l0l34","JM AMF",0,0,NULL,DATE '1987-01-23',now(),now());
+-- DROP DATA POSTS
+INSERT INTO RRSS_Posts (post_id, user_id, comment, type, date_created, date_updated)
+VALUES (0,1,"Hello world","image",now(),now());
+-- DROP DATA COMMENTS
+INSERT INTO RRSS_Comments (comment_id, post_id, user_id, content, date_created, date_updated)
+VALUES (0,0,1,"Love your bald head",now(),now());
 -- DROP DATA FOLLOWING
-INSERT INTO Followings (user_id, following_id, date_created)
+INSERT INTO RRSS_Followings (user_id, following_id, date_created)
 VALUES (0,0,now(),now());
